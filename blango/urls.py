ed0e6e0fa08555443926440b73946ef0c2377a38
Esteban Heredia <estebandheredia@gmail.com>
2022-09-14 18:53:48 +0000

    Finish django configurations

--- blango/urls.py
+++ blango/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
@@ -24,3 +25,6 @@ urlpatterns = [
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
 ]
+
+# from django.conf import settings
+# print(f"Time zone: {settings.TIME_ZONE}")
